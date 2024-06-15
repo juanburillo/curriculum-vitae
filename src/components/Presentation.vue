1f911c6c760cc8ab4a1dd7dff44c7a44476a3944
Juan <juanburillodev@gmail.com>
2024-06-15 11:43:06 +0200

    refactor: :recycle: change margin-bottom to margin-top

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -7,15 +7,15 @@ import data from '../locales/data.json';
     <img src="../assets/portrait.jpg" alt="Portrait of Juan Burillo" class="w-24 h-24 rounded-full">
 
     <div>
-        <div class="mb-6 text-sm text-gray-600">
+        <div class="text-sm text-gray-600">
           <p>{{ data.contact.location }}</p>
           <p>{{ data.contact.phone }} &middot; {{ data.contact.email }}</p>
         </div>
 
-        <div>
+        <div class="mt-6">
           <h1 class="font-bold text-3xl text-gray-800">{{ data.name }}</h1>
-          <h2 class="mb-2 font-medium text-gray-700">{{ data.title }}</h2>
-          <p class="text-lg text-justify text-gray-700">{{ data.description }}</p>
+          <h2 class="font-medium text-gray-700">{{ data.title }}</h2>
+          <p class="mt-2 text-lg text-justify text-gray-700">{{ data.description }}</p>
         </div>
     </div>
   </section>
