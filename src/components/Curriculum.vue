547733be110cb33e5cbe5739220d82cffe123cb3
Juan <juanburillodev@gmail.com>
2024-06-15 11:38:14 +0200

    feat: :sparkles: create Experience component

--- src/components/Curriculum.vue
+++ src/components/Curriculum.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import Experience from './Experience.vue';
 import Presentation from './Presentation.vue';
 </script>
 
 <template>
   <div class="w-[210mm] h-[297mm] my-10 mx-auto p-12 bg-white shadow-md">
     <Presentation />
+
+    <Experience />
   </div>
 </template>
