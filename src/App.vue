8603ab9e32008323ce9dcc9cd4ddfa2dfe582cbc
Juan <juanburillodev@gmail.com>
2024-06-15 22:55:48 +0200

    feat: :sparkles: add custom background to App component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Background from './components/Background.vue';
 import Curriculum from './components/Curriculum.vue';
 </script>
 
 <template>
+  <Background />
   <Curriculum />
 </template>
