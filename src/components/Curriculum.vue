6aefd963488a1336cb9084bf581a8116702a4462
Juan <juanburillodev@gmail.com>
2024-06-15 12:35:19 +0200

    feat: :sparkles: fill Links component

--- src/components/Curriculum.vue
+++ src/components/Curriculum.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
-import Experience from './Experience.vue';
-import Presentation from './Presentation.vue';
-import Education from './Education.vue';
-import Skills from './Skills.vue';
 import Courses from './Courses.vue';
+import Education from './Education.vue';
+import Experience from './Experience.vue';
+import Links from './Links.vue';
+import Presentation from './Presentation.vue';
+import Skills from './Skills.vue';
 </script>
 
 <template>
   <div class="w-[210mm] h-[297mm] my-10 mx-auto p-12 bg-white shadow-md">
     <Presentation />
 
-    <Experience class="mt-8"/>
+    <Experience class="mt-6"/>
 
-    <Education class="mt-8" />
+    <Education class="mt-6" />
 
-    <Skills class="mt-8" />
+    <Skills class="mt-6" />
 
-    <Courses class="mt-8" />
+    <Courses class="mt-6" />
+
+    <Links class="mt-6" />
   </div>
 </template>
--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import data from '../locales/data.json';
 </script>
 
 <template>
+  <div>
+    <h2 class="text-gray-600">Links</h2>
+
+    <div class="mt-2 grid grid-cols-2">
+      <a v-for="social in data.contact.socials" class="font-medium underline" :href="social.url" target="_blank">
+        {{ social.site }}
+      </a>
+    </div>
+  </div>
 </template>
