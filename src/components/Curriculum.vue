1725d24c6d40ecf32dbbc32508110049f74427cf
Juan <juanburillodev@gmail.com>
2024-06-15 11:32:14 +0200

    feat: :sparkles: fill out Presentation component

--- src/components/Curriculum.vue
+++ src/components/Curriculum.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Presentation from './Presentation.vue';
 </script>
 
 <template>
   <div class="w-[210mm] h-[297mm] my-10 mx-auto p-12 bg-white shadow-md">
+    <Presentation />
   </div>
 </template>
--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,6 +1,22 @@
 <script setup lang="ts">
+import data from '../locales/data.json';
 </script>
 
 <template>
+  <section class="flex flex-row gap-x-16">
+    <img src="../assets/portrait.jpg" alt="Portrait of Juan Burillo" class="w-24 h-24 rounded-full">
 
+    <div>
+        <div class="mb-6 text-sm text-gray-600">
+          <p>{{ data.contact.location }}</p>
+          <p>{{ data.contact.phone }} &middot; {{ data.contact.email }}</p>
+        </div>
+
+        <div>
+          <h1 class="font-bold text-3xl text-gray-800">{{ data.name }}</h1>
+          <h2 class="mb-2 font-medium text-gray-700">{{ data.title }}</h2>
+          <p class="text-lg text-justify text-gray-700">{{ data.description }}</p>
+        </div>
+    </div>
+  </section>
 </template>
