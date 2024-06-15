4947b9ce399fbec84b5cba7c8506908157102384
Juan <juanburillodev@gmail.com>
2024-06-15 22:56:20 +0200

    fix: :lipstick: fix custom background height

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -2,6 +2,6 @@
 </script>
 
 <template>
-  <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+  <div class="fixed inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
   </div>
 </template>
