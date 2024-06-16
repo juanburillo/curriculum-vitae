213f5d944f560e68982be275dde6ed7410d6c4e9
Juan <juanburillodev@gmail.com>
2024-06-16 16:55:43 +0200

    feat: :sparkles: add I18n plugin to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
+import { i18n } from './plugins/i18n'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
