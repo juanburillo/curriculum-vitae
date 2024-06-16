3444551c014d9989f65e559fe277bb0287a96800
Juan <juanburillodev@gmail.com>
2024-06-16 02:37:37 +0200

    chore: :fire: remove data.json from Presentation component

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import data from '../locales/data.json';
 </script>
 
 <template>
@@ -8,14 +7,17 @@ import data from '../locales/data.json';
 
     <div class="w-5/6">
         <div class="text-sm text-gray-600">
-          <p>{{ data.contact.location }}</p>
-          <p>{{ data.contact.phone }} &middot; {{ data.contact.email }}</p>
+          <p>Madrid, Spain</p>
+          <p>+34 648 09 24 23 &middot; juanburillodev@gmail.com</p>
         </div>
 
         <div class="mt-6">
-          <h1 class="font-bold text-3xl text-gray-800">{{ data.name }}</h1>
-          <h2 class="font-medium text-gray-700">{{ data.title }}</h2>
-          <p class="mt-2 text-lg text-justify text-gray-700">{{ data.description }}</p>
+          <h1 class="font-bold text-3xl text-gray-800">Juan Burillo Retuerto</h1>
+          <h2 class="font-medium text-gray-700">Full Stack Developer</h2>
+          <p class="mt-2 text-lg text-justify text-gray-700">
+            I am a junior web developer with a strong desire to join the professional world.
+            I enjoy continuously learning and learning new technologies, as well as deepening my knowledge of those I already know.
+          </p>
         </div>
     </div>
   </section>
