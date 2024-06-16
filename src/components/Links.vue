ed7a631fece51dc8314689d1375cd6db45a282a5
Juan <juanburillodev@gmail.com>
2024-06-16 02:48:30 +0200

    chore: :fire: remove data.json from Links component

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import data from '../locales/data.json';
 </script>
 
 <template>
   <section class="flex gap-x-8">
     <h2 class="w-1/6 text-gray-600">Links</h2>
 
-    <div class="w-5/6 flex gap-x-52">
-      <a v-for="social in data.contact.socials" class="font-medium underline" :href="social.url" target="_blank">
-        {{ social.site }}
+    <div class="w-5/6 flex gap-x-52 font-medium underline">
+      <a href="https://www.linkedin.com/in/juan-burillo/" target="_blank">
+        LinkedIn
+      </a>
+      <a href="https://github.com/juanburillo/" target="_blank">
+        GitHub
       </a>
     </div>
   </section>
