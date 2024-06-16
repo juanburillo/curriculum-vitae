0239854e59edcca19e6cfa475ed4c7472e953190
Juan <juanburillodev@gmail.com>
2024-06-16 16:46:47 +0200

    chore: :wrench: add Vue I18n plugin to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { dirname, resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/curriculum-vitae/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
+    }),
+  ],
 })
