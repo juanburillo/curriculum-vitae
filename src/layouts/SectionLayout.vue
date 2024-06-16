110310aa8ec8428a73a743cf3f7c3191478c6663
Juan <juanburillodev@gmail.com>
2024-06-16 12:26:54 +0200

    feat: :sparkles: write SectionLayout code

--- src/layouts/SectionLayout.vue
+++ src/layouts/SectionLayout.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
 </script>
 
 <template>
+  <section class="flex gap-x-8">
+    <h2 class="w-1/6 text-gray-600">{{ title }}</h2>
+
+    <div class="w-5/6">
+      <slot />
+    </div>
+  </section>
 </template>
