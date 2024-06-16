0fffcc73620fb5163f1088c10f34560c427a73ae
Juan <juanburillodev@gmail.com>
2024-06-16 19:24:06 +0200

    feat: :sparkles: create locale switch functionality

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Background from './components/Background.vue';
 import Curriculum from './components/Curriculum.vue';
+import LocaleLinks from './components/LocaleLinks.vue';
 </script>
 
 <template>
   <Background />
   <Curriculum />
+  <LocaleLinks />
 </template>
--- src/components/LocaleLinks.vue
+++ src/components/LocaleLinks.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n();
+
+const changeLocale = (lang: string) => {
+  locale.value = lang;
+};
 </script>
 
 <template>
+  <div class="mb-10 flex justify-center gap-x-10">
+    <p @click="changeLocale('es')" class="underline cursor-pointer hover:opacity-70 transition-all duration-150 ease-in-out">Español</p>
+    <p @click="changeLocale('en')" class="underline cursor-pointer hover:opacity-70 transition-all duration-150 ease-in-out">English</p>
+  </div>
 </template>
