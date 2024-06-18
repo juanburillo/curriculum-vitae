954d942acc0cc83b599f7c30dd1c2d3973859360
Juan <juanburillodev@gmail.com>
2024-06-18 15:17:22 +0200

    chore: :lipstick: change custom background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -2,6 +2,5 @@
 </script>
 
 <template>
-  <div class="fixed inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-  </div>
+  <div class="fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 </template>
